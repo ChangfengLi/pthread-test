7f69943844bb86976e9997c48c13acbb7da9fa00
ChangfengLi <1278845487@qq.com>
2020-09-20 17:28:38 +0800

    add test2.c

--- makefile
+++ makefile
@@ -3,4 +3,5 @@ all:
 	@echo "Start compiling......"
 	@echo ""
 	gcc -o test1 test1.c -lpthread
+	gcc -o test2 test2.c -lpthread
 	@echo "end"
--- test1.c
+++ test1.c
@@ -3,33 +3,37 @@
 #include <stdlib.h>
 
 void print_message_func(void *ptr);
+void print_message_func2(void *ptr);
 
 int main()
 {
     int tmp1,tmp2;
     void *retival;
     pthread_t thread1,thread2;
-    char *message1 = "thread1";
-    char *message2 = "thread2";
+    char *message1 = "thread0";
+    char *message2 = "thread1";
     
     int ret_thread1,ret_thread2;
     ret_thread1 = pthread_create(&thread1,NULL,(void *)&print_message_func,(void *)message1);
-    ret_thread2 = pthread_create(&thread2,NULL,(void *)&print_message_func,(void *)message2);
+    ret_thread2 = pthread_create(&thread2,NULL,(void *)&print_message_func2,(void *)message2);
+
     
+
     if(ret_thread1 == 0)
         printf("create thread 1 true\n");
     else
         printf("create thread 1 false\n");
-    
+
     if(ret_thread2 == 0)
         printf("create thread 2 true\n");
     else
         printf("create thread 2 false\n");
     
-    tmp1 = pthread_join(thread1,&retival);
+
     printf("thread 1 return value (retival) is %d\n",(int)retival);
     printf("thread 1 return value (tmp1) is %d\n",tmp1);
     
+    tmp1 = pthread_join(thread1,&retival);
     if(tmp1 != 0)
         printf("cannot join with thread 1\n");
     printf("thread 1 is end\n");
@@ -46,6 +50,13 @@ int main()
 
 void print_message_func(void *ptr)
 {
+    for(int i=0;i<1000;++i)
+        printf("%s:%d\n",(char*)ptr,i);
+}
+
+void print_message_func2(void *ptr)
+{
     for(int i=0;i<5;++i)
         printf("%s:%d\n",(char*)ptr,i);
 }
+
